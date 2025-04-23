38bd78ffafd7667fbb870404b86aae40306b016d
kvietmeier <karlv@storagenet.org>
2025-04-23 21:53:30 +0000

    added template

---
+++ header_template.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Script Name: <script-name>.sh
+# Description: <Brief description of what the script does>
+#
+# Author: <Your Name>
+# License: Apache License, Version 2.0
+# License URL: http://www.apache.org/licenses/LICENSE-2.0
+#
+# This script is licensed under the Apache License, Version 2.0.
+# You may not use this file except in compliance with the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Usage:
+#   ./<script-name>.sh [option]
+#
+# Options:
+#   <option1>    Description of option1
+#   <option2>    Description of option2
+#   ...
